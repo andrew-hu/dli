97c0715b8760b6a48cd10f4d50f47d04d811b34b
RanaShamoun <rana200035@yahoo.com>
2016-03-22 10:11:37 -0700

    line 3

--- hello.php
+++ hello.php
@@ -3,7 +3,9 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
-<?php echo '<p>Hey Guys</p>'; ?> 
+ <?php echo '<p>Hello World</p>'; ?>
+<?php echo '<p>Hey Guys</p>'; ?>
+<?php echo '<p>Hey Guys</p>'; ?>
+
  </body>
 </html>
