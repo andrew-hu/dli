64318ebcbcbe122777fa4fc018d9e5c6ba2bbc35
andrew-hu <andrew.hu@seven-26.com>
2016-03-30 15:31:12 -0700

    css2
    css

--- homepage.php
+++ homepage.php
@@ -6,24 +6,28 @@
 		<link rel="stylesheet" type="text/css" href="css/homepage.css">
 	</head>
 	<body>
-		<div>
-			<h1>DLI</h1>
-		</div>
-		<div>
-			<a href="translator.php">Translator</a>
-		</div>
-		<div>
-			<a href="ettiquite.php">Ettiquite</a>
-		</div>
-		<div>
-			<a href="taste-of-home.php">Taste of Home</a>
-		</div>
-		<div>
-			<a href="meetups.php">Meetups</a>
-		</div>
-		<div>
-			<a href="contact-us.php">Contact Us</a>
-		</div>
-
+		<!--div id="wrapper"-->
+			<header id="header">
+				<h1 class="logo"><a href="homepage.php"><img src="http://i.imgur.com/47112La.png?1"></a></h1>
+				<!--span class="header-description">Bringing Communities Together<span-->
+			</header>
+			<div id="container">
+				<a href="translator.php"><div class="select fade">
+					Translator
+				</div>
+				<a href="ettiquite.php"><div class="select fade">
+					Ettiquite
+				</div></a>
+				<a href="taste-of-home.php"><div class="select">
+					Taste of Home
+				</div></a>
+				<a href="meetups.php"><div class="select">
+					Meetups
+				</div></a>
+				<a href="contact-us.php"><div class="select">
+					Contact Us
+				</div></a>
+			</div>
+		<!--/div-->
 	</body>
 </html>
