f30b0cac255f8318708dd7f147c880ba8496d05f
RanaShamoun <rana200035@yahoo.com>
2016-04-06 10:55:54 -0700

    4/6 Changes
    Added cacert.pem for php, ettiquite changes, OAuth lib folder, and
    sample.php for yelp api.

--- ettiquite.php
+++ ettiquite.php
@@ -7,7 +7,96 @@
 			<h1>DLI Ettiquite Guide</h1>
 		</div>
 		<div>
-			Info here
+		    <img src="http://www.resourcesforlife.com/wp/wp-content/uploads/2015/01/20150119tu-Diplomacy-US-President-George-W-Bush-holds-hand-with-king-saudi-arabia-6446422.jpg"
+            width="596" height="387"></img><br></br>
+			American:It is very typical for people in America to dress casually. You aren't required to treat everyone like you're at a business meeting.
+			 You don't have to stay professional at all times.<br></br>
+			Middle East: It is abnormal to not shake hands with people. Kiss on the cheek is only for women and it is abnormal to touch people who aren't your relatives. It is seen as disrespectful.
+			<br></br>
+			arabic translation<br></br>
+		</div>
+		<div>
+		    <img src="http://connxn.net/cnxn/wp-content/uploads/2012/08/2-people-talking-photo.jpeg"
+           width="586" height="389"><br></br>
+			American: don't stand too close.When talking with others, you don't really have any requirements besides not being too close to them.
+			Being too close can cause discomfort. You can also use a very monotone voice and don't need body language. <br></br>
+			Middle East: When talking with others, you need to be very expressive; you must use a loud voice paired up with body movements,
+			 and must keep an arm's length of distance between the people speaking.<br></br>
+			arabic translation<br></br>
+		</div>
+		<div>
+		    <img style="-webkit-user-select: none" src="http://www.mthornehr.co.uk/wp-content/uploads/2013/10/business-meeting-two-people.jpg"
+            width="586" height="389"><br></br>
+			American and Middle East culture both use direct eye contact while speaking. <br></br>
+			arabic translation<br></br>
+			 <br></br>
+			 <div>
+            <img src="http://www.careerealism.com/wp-content/uploads/2012/11/Dream-Late-Featured.png"
+             width="586" height="389"><br></br>
+            American: In American culture, everyone must be on time. being on time is vital. You can lose your job if you're late on anything.Being late to an event is not nice and shows that people are not respecting others time. <br></br>
+			Middle East: In Syrian culture, you can be late for up to 30 minutes. It is seen as the normal to not be on time, and everyone respects that mutually. Though it is good to be on time, meetings usually won't start until all parties are present.<br></br>
+			arabic translation<br></br>
+		</div>
+		<div>
+        <img src="http://businessmodes.com/wp-content/uploads/2012/05/Business-Meeting.jpg"
+        width="586" height="389"><br></br>
+        American: In America,meetings are straight forward and usually one speaker at a time. You can have informal meetings, which aren't that important but mandatory. Cooperation with co-workers is good. <br></br>
+        Middle East:Meetings are very back and fourt; many people contribute to the discussions. <br></br>
+        arabic translation<br></br>
+		</div>
+		<div>
+		     <img src="http://www.careerealism.com/wp-content/uploads/2011/11/6-tips-for-working-women-with-families.jpg"
+		     width="586" height="389"><br></br>
+			American: America is moving towards gender equality and feminism, therefore making working women a normal thing in culture.<br></br>
+			Middle East: You can work as a woman, but it is looked down upon as you are supposed to stay at home.<br></br>
+			arabic translation<br></br>
+		</div>
+		<div>
+        <img src="http://cdn.images.express.co.uk/img/dynamic/11/590x/Child-holding-cigarette-and-lighter-560699.jpg"
+		    width="586" height="389"><br></br>
+        American: The legal age for smoking cigarettes is 21. The legal age for consuming alcohol is 21. These rules don't really matter since most people do both before 18 anyways. Do not do any in public.  <br></br>
+        Middle East:The legal age to smoke and drink is 18, most people don't follow this rule. You can smoke and not be looked down upon.<br></br>
+        arabic translation<br></br>
+		</div>
+		<div>
+		 <img src="https://www.naela.org/app_themes/public/images/NAELA%20Images/Membership%20Images/j0433151.jpg"
+         width="586" height="389"><br></br>
+        American:Business attire usually consists of suits and dresses, but there is no need to overdress. You don't need to wear a tuxedo.  <br></br>
+        Middle East:Business attire consists of suits and dresses.<br></br>
+        arabic translation<br></br>
+		</div>
+        <div>
+        <img src="http://www.gradhacker.org/wp-content/uploads/2012/09/WiersmaEngagingLecture.gif"width="586" height="389"><br></br>
+        American: In school, the most common way of teaching is through lecture.  <br></br>
+        Middle East:Same as America.<br></br>
+        arabic translation<br></br>
+		</div>
+		<div>
+		    <img src="http://picture-cdn.wheretoget.it/ms82na-l-610x610-jewels-mr-mrs-cups.jpg"
+        width="586" height="389"><br></br>
+        American:When addressing a person, you call them by (Mr., Ms., Mrs.,) their last name. You can call them by their first name if told.   <br></br>
+        Middle East:Same as America.<br></br>
+        arabic translation<br></br>
+		</div>
+		<div>
+        <img src="http://i.huffpost.com/gen/1283675/thumbs/o-COUPLE-HOLDING-HANDS-facebook.jpg" width="586" height="389"><br></br>
+        American: You can have sex with anyone you want, as long as it's legal and both parties consent. Smoking is accepted, but you must go to certain areas to smoke. <br></br>
+        Middle East:Sex before marriage is looked down upon, and very uncommon. Smoking in public is common, and you usually don't have to leave a restaurant to smoke. <br></br>
+        arabic translation<br></br>
+		</div>
+		<div>
+		    width="586" height="389"><br></br>
+        American: In American culture, it is seen as normal to do anything. You can get away with a lot of things. The only thing looked down upon is touching people of the same sex in certain ways.<br></br>
+        Middle East: It is offensive to point your heel or toe at someone. You cannot display and affection, that is reserved for private places. You cannot act sexually in public, that is significantly offensive.
+        Homosexuality is looked down upon since Syrian culture is strongly religious.<br></br>
+        Arabic Translation: <br></br>
+		</div>
+		<div>
+		    width="586" height="389"><br></br>
+			American: The only thing that can offend American people is sticking your middle finger at someone. Men and women are usually sitting with legs fully crossed, and smiling at everyone else. <br></br>
+			Middle East:Tilting your head back and raising your eyebrows is a physical gesture of "no".<br></br>
+			arabic translation<br></br>
+		</div>
 		</div>
 
 
