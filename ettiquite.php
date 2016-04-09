270421894fccdcd600d55d961b2dc5b130de52cc
Rana Shamoun <rana200035@yahoo.com>
2016-04-08 22:56:33 -0700

    change ettiquite
    I completed the ettiquite page.

--- ettiquite.php
+++ ettiquite.php
@@ -6,6 +6,7 @@
 	<body>
 		<div>
 			<h1>DLI Ettiquite Guide</h1>
+			<h1> دليل آداب</h1>
 		</div>
 		<div>
 		    <img src="http://www.resourcesforlife.com/wp/wp-content/uploads/2015/01/20150119tu-Diplomacy-US-President-George-W-Bush-holds-hand-with-king-saudi-arabia-6446422.jpg"
@@ -14,7 +15,13 @@
 			 You don't have to stay professional at all times.<br></br>
 			Middle East: It is abnormal to not shake hands with people. Kiss on the cheek is only for women and it is abnormal to touch people who aren't your relatives. It is seen as disrespectful.
 			<br></br>
-			arabic translation: مرحبا<br></br>
+			Arabic translation:امريكا: من المعتاد جدا للناس في أمريكا ان يلبسوا لباس غير رسمي في الايام العادية.  ليس من الضروري ان تعامل الجميع  كانهم  في اجتماع عمل. ليس هناك حاجة ان يلبسوا ملابس رسمية في الايام العادية .
+            <br></br>
+            الشرق الأوسط: إنه أمر غير طبيعي لعدم مصافحة الناس. قبلة على الخد تمارسها  النساء فقط، وأنه أمر غير طبيعي لمس الناس الذين ليسوا من أقاربك. ينظر إليها على أنها غير محترمة.
+
+
+
+        <br></br>
 		</div>
 		<div>
 		    <img src="http://connxn.net/cnxn/wp-content/uploads/2012/08/2-people-talking-photo.jpeg"
@@ -23,80 +30,102 @@
 			Being too close can cause discomfort. You can also use a very monotone voice and don't need body language. <br></br>
 			Middle East: When talking with others, you need to be very expressive; you must use a loud voice paired up with body movements,
 			 and must keep an arm's length of distance between the people speaking.<br></br>
-			arabic translation<br></br>
+			Arabic translation: أمريكا: لا تقف قريبا جدا عند تحدث مع الآخرينزالقرب كثيرا عند التحدث مع الاخرين يسبب مضايقتهم ليس من الضروري لمس او رفع الصوت عند المخاطبة مع الاخرين.
+            <br></br>
+            الشرق الأوسط: عندما نتحدث مع الآخرين، عليك أن تكون معبر جدا. يجب عليك استخدام صوت عال  مع حركات الجسم، ويجب أن تبقى طول ذراع من المسافة بين الناس عند تحدث.
+		<br></br>
 		</div>
 		<div>
 		    <img style="-webkit-user-select: none" src="http://www.mthornehr.co.uk/wp-content/uploads/2013/10/business-meeting-two-people.jpg"
             width="586" height="389"><br></br>
 			American and Middle East culture both use direct eye contact while speaking. <br></br>
-			arabic translation<br></br>
+			Arabic translation:امريكا والشرق الأوسط على حد سواء يستخدمون الاتصال المباشر وجها لوجه عند تحدث مع البعض<br></br>
 			 <br></br>
 			 <div>
             <img src="http://www.careerealism.com/wp-content/uploads/2012/11/Dream-Late-Featured.png"
              width="586" height="389"><br></br>
             American: In American culture, everyone must be on time. being on time is vital. You can lose your job if you're late on anything.Being late to an event is not nice and shows that people are not respecting others time. <br></br>
 			Middle East: In Syrian culture, you can be late for up to 30 minutes. It is seen as the normal to not be on time, and everyone respects that mutually. Though it is good to be on time, meetings usually won't start until all parties are present.<br></br>
-			arabic translation<br></br>
+
+			Arabic translation: في  امريكا يجب أحترام الوقت يجب على الجميع الحضور في الوقت المحدد. يمكنك أن تفقد وظيفتك إذا كنت  متأخر في يوم ما.<br></br>
+		الشرق الأوسط: في الثقافة السورية، يمكنك أن تكون متأخر لمدة تصل إلى ٣٠ دقيقة. وينظر إليه على أنه من امر طبيعي وعادة الاجتمعات  لا تبدا الا بعد حضور الجميع . <br></br>
 		</div>
 		<div>
         <img src="http://businessmodes.com/wp-content/uploads/2012/05/Business-Meeting.jpg"
         width="586" height="389"><br></br>
         American: In America,meetings are straight forward and usually one speaker at a time. You can have informal meetings, which aren't that important but mandatory. Cooperation with co-workers is good. <br></br>
-        Middle East:Meetings are very back and fourt; many people contribute to the discussions. <br></br>
-        arabic translation<br></br>
-		</div>
+        Middle East:Meetings are very back and forth; many people contribute to the discussions. <br></br>
+        Arabic translation: في أمريكا، والاجتماعات هي على التوالي إلى الأمام، وعادة ما متكلم واحد يتكلم في وقت واحد. من ممكن أن تكون الاجتماعات غير الرسمية، التي ليست ذات أهمية  ولكنها إلزامية. التعاون مع زملاء هو صفة جيدة.<br></br>
+		الشرق الأوسط: اجتماعات غير منظمة. يساهم العديد من الناس في المناقشات في نفس الوقت.</div>
+		<br></br>
 		<div>
 		     <img src="http://www.careerealism.com/wp-content/uploads/2011/11/6-tips-for-working-women-with-families.jpg"
 		     width="586" height="389"><br></br>
 			American: America is moving towards gender equality and feminism, therefore making working women a normal thing in culture.<br></br>
 			Middle East: You can work as a woman, but it is looked down upon as you are supposed to stay at home.<br></br>
-			arabic translation<br></br>
+			Arabic translation:أمريكا تتجه نحو تحقيق المساواة بين الجنسين والنسوية، مما يجعل النساء العاملات شيء طبيعي في الثقافة الامريكية.
+			<br></br>
+			الشرق الأوسط: من ممكن أن تعمل  المرأة خارج المنزل، ولكن  من المفترض عليها البقاء في المنزل.
+			<br></br>
+
 		</div>
 		<div>
         <img src="http://cdn.images.express.co.uk/img/dynamic/11/590x/Child-holding-cigarette-and-lighter-560699.jpg"
 		    width="586" height="389"><br></br>
-        American: The legal age for smoking cigarettes is 21. The legal age for consuming alcohol is 21. These rules don't really matter since most people do both before 18 anyways. Do not do any in public.  <br></br>
-        Middle East:The legal age to smoke and drink is 18, most people don't follow this rule. You can smoke and not be looked down upon.<br></br>
-        arabic translation<br></br>
+        American: The legal age for smoking cigarettes is 21. The legal age for consuming alcohol is 21. These rules don't really matter since most people do both before 18 anyways. Do not do any in public.You must go to certain areas to smoke.  <br></br>
+        Middle East:The legal age to smoke and drink is 18, most people don't follow this rule. You can smoke and not be looked down upon.Smoking in public is common, and you usually don't have to leave a restaurant to smoke.<br></br>
+        Arabic translation:أمريكا: السن القانوني للتدخين السجائر هو ٢١. السن القانوني للاستهلاك الكحول هو ٢١. هذه القواعد لا تهم حقا لأن كثير من الناس قبل ١٨ تبدا بالتدخين ولكن لا تمارس التدخين في الأماكن العامة ومن المقبول التدخين، ولكن يجب أن تذهب إلى بعض المناطق في التدخين.
+.<br></br>
+
+		الشرق الأوسط: السن القانوني للتدخين وشرب هو ١٨، ومعظم الناس لا تتبع هذه القاعدة. يمكنك التدخين ولا ينظر اليه بازدراء.
+		التدخين في الأماكن العامة هو شائع، ولا يتوجب مغادرة  مطعم من اجل التدخين.<br></br>
 		</div>
 		<div>
 		 <img src="https://www.naela.org/app_themes/public/images/NAELA%20Images/Membership%20Images/j0433151.jpg"
          width="586" height="389"><br></br>
         American:Business attire usually consists of suits and dresses, but there is no need to overdress. You don't need to wear a tuxedo.  <br></br>
         Middle East:Business attire consists of suits and dresses.<br></br>
-        arabic translation<br></br>
+        Arabic translation:في امريكا  زي الأعمال يتكون من البدلات الرسمية في بعض الاحيان ولكن ليس هناك حاجة إلى تبرج الأمريكي لا يحتاج إلى ارتداء ملابس السهرة للرجال لذهاب الى العمل.<br></br>
+		الشرق الأوسط: ويتكون زي الأعمال من البدلات الرسمية.
+		<br></br>
 		</div>
         <div>
-        <img src="http://www.gradhacker.org/wp-content/uploads/2012/09/WiersmaEngagingLecture.gif"width="586" height="389"><br></br>
+        <img  src="http://garfieldparkacademy.org/wp-content/themes/infocus/lib/scripts/timthumb/thumb.php?src=http://garfieldparkacademy.org/wp-content/uploads/2013/03/rotator1.jpg&amp;w=600&amp;h=340&amp;zc=1&amp;q=100" width="586" height="389"><br></br>
         American: In school, the most common way of teaching is through lecture.  <br></br>
         Middle East:Same as America.<br></br>
-        arabic translation<br></br>
+        Arabic translation:أمريكا وفي الشرق الأوسط في المدرسة الطريقة الأكثر شيوعا للتعليم هي من خلال المحاضرة.
+
+<br></br>
 		</div>
 		<div>
 		    <img src="http://picture-cdn.wheretoget.it/ms82na-l-610x610-jewels-mr-mrs-cups.jpg"
         width="586" height="389"><br></br>
         American:When addressing a person, you call them by (Mr., Ms., Mrs.,) their last name. You can call them by their first name if told.   <br></br>
         Middle East:Same as America.<br></br>
-        arabic translation<br></br>
+        Arabic translation:أمريكا وفي الشرق الأوسط: عند مخاطبة شخص، يمكنك استدعاءهم بلقب (السيد، السيدة) الاسم الأخير. يمكنك  مخاطبة شخص  باسم الأول إذا سمح بذلك.<br></br>
 		</div>
 		<div>
         <img src="http://i.huffpost.com/gen/1283675/thumbs/o-COUPLE-HOLDING-HANDS-facebook.jpg" width="586" height="389"><br></br>
-        American: You can have sex with anyone you want, as long as it's legal and both parties consent. Smoking is accepted, but you must go to certain areas to smoke. <br></br>
-        Middle East:Sex before marriage is looked down upon, and very uncommon. Smoking in public is common, and you usually don't have to leave a restaurant to smoke. <br></br>
-        arabic translation<br></br>
+        American: You can have sex with anyone you want, as long as it's legal and both parties consent. <br></br>
+        Middle East:Sex before marriage is looked down upon, and very uncommon. <br></br>
+        Arabic translation: أمريكا: من المكن  ممارسة الجنس مع أي شخص تريد، طالما انها قانونية وكلا  الطرفين موافقي<br></br>
+        الشرق الأوسط: الجنس قبل الزواج ينظرون إليه باحتقار، وغير المألوف للغاية
+        <br></br>
 		</div>
 		<div>
-		    width="586" height="389"><br></br>
+		  <img src="http://www.theyellowsideoflife.com/wp-content/uploads/2015/09/Non-sexual-intimacy.jpg"  width="586" height="389"><br></br>
         American: In American culture, it is seen as normal to do anything. You can get away with a lot of things. The only thing looked down upon is touching people of the same sex in certain ways.<br></br>
         Middle East: It is offensive to point your heel or toe at someone. You cannot display and affection, that is reserved for private places. You cannot act sexually in public, that is significantly offensive.
         Homosexuality is looked down upon since Syrian culture is strongly religious.<br></br>
-        Arabic Translation: <br></br>
+        Arabic Translation:في الثقافة الأمريكية ينظر إلى عديد من الاشياء بصورة مقبولة . ولكن الشيء الوحيدالذي ينظر له بازدراء هو لمس الناس من نفس الجنس بطرق معينة.<br></br>
+         الشرق الأوسط: من المهين أن نشير الكعب أو أخمص القدمين في شخص ما. لا يمكنك عرض والمودة في اماكن عامة فهي محفوظة لأماكن خاصة. أنت لا تستطيع أن تتصرف الجنس في الأماكن العامة، وهذا الشي مرفوض بشدة من قبل المجتمع السوري. <br></br>
 		</div>
 		<div>
-		    width="586" height="389"><br></br>
+		   <img  src="http://3.bp.blogspot.com/-oCZ9v9bqN0s/Tzc_YwrChuI/AAAAAAAACAk/3cP9YWaaxCQ/s400/skeptical%2Bbaby%2Bmeme%2Bplain.png"   width="586" height="389"><br></br>
 			American: The only thing that can offend American people is sticking your middle finger at someone. Men and women are usually sitting with legs fully crossed, and smiling at everyone else. <br></br>
 			Middle East:Tilting your head back and raising your eyebrows is a physical gesture of "no".<br></br>
-			arabic translation<br></br>
+			Arabic translation:أمريكا: الشيء الوحيد الذي يمكن أن تسيء به الشعب الأميركي هو اشارة إصبعك الأوسط على شخص ما. الرجال والنساء عادة ما يجلس مع الساقين عبرت تماما، ويبتسم في الجميع.<br></br>
+         الشرق الأوسط: إمالة رأسك إلى الخلف ورفع حاجبيك هي إيماءة جسدية  معناها لا<br></br>
 		</div>
 		</div>
 
